f8d49f1ee5c08db899852010e821426b1e1d7d7b
cksgud0403 <42197731+cksgud0403@users.noreply.github.com>
2023-07-26 17:43:06 +0900

    feat: 조회한 빈이 모두 필요할 때, List, Map

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -4,7 +4,7 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
+@Component
 @Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
 
